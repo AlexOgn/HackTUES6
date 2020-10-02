986f2522ead44c4b7682350a441de1ddfddcbf52
Theodor Totev <teoet6@protonmail.com>
2020-10-02 16:11:26 +0300

    Fixed pathfind.js output being backwards

--- pathfind.js
+++ pathfind.js
@@ -8,7 +8,7 @@ const compare = (a, b) => JSON.stringify(a) == JSON.stringify(b);
 const pathfind = (city, src, dest) => {
     const add_pos = (a, b) => ({x: a.x + b.x, y: a.y + b.y});
     let bfsq = [];
-    bfsq.push({pos:dest, path:[]});
+    bfsq.push({pos:src, path:[]});
     while(bfsq.length > 0) {
         let front = bfsq.shift();
         let pos = front.pos;
@@ -17,8 +17,8 @@ const pathfind = (city, src, dest) => {
         if (city[pos.y] === undefined) continue;
         if (city[pos.y][pos.x] === undefined) continue;
 
-        if (compare(pos, src)) return path;
-        if (!compare(pos,  dest) && city[pos.y][pos.x].type == "Street");
+        if (compare(pos, dest)) return path;
+        if (!compare(pos,  src) && city[pos.y][pos.x].type == "Street");
         bfsq.push({
             pos:add_pos(pos, {x: 1, y:0}),
             path:path
