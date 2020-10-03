8b36f3e0fd1b46ebf9e048f0385b62342152b2d3
teoet6 <teoet6@protonmail.com>
2020-10-03 23:02:03 +0300

    removed canvas hack

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var game_canvas = document.getElementById("game_canvas");
-game_canvas.width = window.innerWidth / 2;
-game_canvas.height = window.innerHeight / 2;
+//game_canvas.width = window.innerWidth / 2;
+//game_canvas.height = window.innerHeight / 2;
 var game_ctx = game_canvas.getContext("2d");
 
 var game_mouse_x;
