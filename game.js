2250470fff2da04ff54d62d5760556baae2c8c1e
Theodor Totev <teoet6@protonmail.com>
2020-10-03 11:21:14 +0300

    Added street drawing. I grew up on the street so it was very intuitive.

--- game.js
+++ game.js
@@ -1,30 +1,54 @@
 var game_canvas = document.getElementById("game-canvas");
+game_canvas.width = window.innerWidth;
+game_canvas.height = window.innerHeight;
 var game_ctx = game_canvas.getContext("2d");
 
 const draw_street = (ctx, map, size, y, x) => {
-    //TODO
+    ctx.fillStyle = "Gray";
+    let half = size / 2;
+    let quart = half / 2;
+    const can_connect = (diry, dirx) => (undefined_member(map, y + diry, x + dirx) !== undefined);
+    const draw_arm = (y, x) => {
+        ctx.save();
+        ctx.fillRect(quart, quart, half, half);
+        ctx.translate(Math.abs(y) * quart, Math.abs(x) * quart);
+        y = (y + 1) * y;
+        x = (x + 1) * x;
+        ctx.fillRect(x * quart , y * quart, half, half);
+        ctx.restore();
+    };
+    if (can_connect( 0,  1)) draw_arm( 0,  1);
+    if (can_connect( 0, -1)) draw_arm( 0, -1);
+    if (can_connect( 1,  0)) draw_arm( 1,  0);
+    if (can_connect(-1,  0)) draw_arm(-1,  0);
+    
 }
 
 const draw_map = (ctx, map, size) => {
     for (let y = 0; y < map.length; y++) {
         for(let x = 0; x < map[y].length; x++) {
-            if (undefined_member(map, y, x) === undefined) continue;
-            if (map[y][x].type == "Factory")
-                ctx.drawImage(assets.factory_img, x * size, y * size, size, size);
-            else if (map[y][x].type == "House")
-                ctx.drawImage(assets.house_img, x * size, y * size, size, size);
+            ctx.save();
+            ctx.translate(x*size, y*size);
+            ctx.fillStyle = "Green";
+            ctx.fillRect(0, 0, size, size);
+            if (undefined_member(map, y, x) === undefined) {ctx.restore(); continue;}
             draw_street(ctx, map, size, y, x);
+            if (map[y][x].type == "Factory")
+                ctx.drawImage(assets.factory_img, 0, 0, size, size);
+            else if (map[y][x].type == "House")
+                ctx.drawImage(assets.house_img, 0, 0, size, size);
+
+            ctx.restore();              
         }
     }
 }
 
 const redraw = () => {
-    //game_ctx.save();
+    game_ctx.save();
     game_ctx.setTransform(1, 0, 0, 1, 0, 0);
-    game_ctx.clearRect(0, 0, game_canvas.width, game_canvas.height);
-    //game_ctx.restore();
-    draw_map(game_ctx, test, 50);
+    //game_ctx.clearRect(0, 0, game_canvas.width, game_canvas.height);
+    game_ctx.restore();
+    draw_map(game_ctx, test, 100);
     window.requestAnimationFrame(redraw);
 }
-
 redraw();
