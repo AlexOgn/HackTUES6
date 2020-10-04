2e1a8cada5b1a5a3e0d667681abd32f8ca4a83bf
KrisiPisi <kristianm2005@gmail.com>
2020-10-05 01:03:17 +0300

    made the score fill in automatically

--- client/mainloop.js
+++ client/mainloop.js
@@ -9,11 +9,14 @@ const mainloop = () => {
     delay_counter++;
     if (delay_counter % delay != 0) return;
     lived++;
+    document.getElementById("score").value = lived;
     document.getElementById("lived").innerHTML = lived + "минута";
     [build_map, car_map] = tick(build_map, car_map);
     if(lived % factory_freq == 0) build_map = add_factory(build_map);
     if(lived % house_freq == 0) build_map = add_house(build_map);
     if(lived % garage_freq == 0) build_map = exit_garage(build_map);
+    
+
 }
 
 window.setInterval(mainloop, 100);
